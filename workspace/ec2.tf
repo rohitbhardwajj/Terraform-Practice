deecd86716328555ba4aeece80e44863016ee62e
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2026-09-05 11:42:42 +0530

    chnage main instance type

---
+++ workspace/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "automate-instance" {
+  ami           = "ami-0b6d9d3d33ba97d99"
+
+  
+  instance_type = "t3.small"
+  tags = {
+    Name = "automate-instance"
+  }
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp2"
+  }
+}
